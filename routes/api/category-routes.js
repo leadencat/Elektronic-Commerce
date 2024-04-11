4097db16ae415fae139f353e185b5ba0ce8cc164
leadencat <jdanders1999@gmail.com>
2024-04-11 13:25:44 -0400

    added some routes in category folder

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,14 +3,35 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+    try {
   // find all categories
+  const categoryData = await Category.findAll({
   // be sure to include its associated Products
+  include: [{ model: Product, as: 'products'}],
+});
+res.status(200).json(categoryData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+    try {
   // find one category by its `id` value
+  const categoryData = await Category.findByPk(req.params.id, {
   // be sure to include its associated Products
+  include: [{ model: Product, as: 'products'}],
+});
+
+if (!categoryData) {
+    res.status(404).json({ message: 'No category found with ID'});
+    return;
+}
+res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 router.post('/', (req, res) => {
